95ab5f2acf1e26d96ab2f162b9a0d3196dcce246
fabiovincenzi <fabiovincenzi2@studio.unibo.it>
2023-01-21 12:53:22 +0100

    selection of points

--- src/main/java/com/ds4h/view/CornerSelectorGUI.java
+++ src/main/java/com/ds4h/view/CornerSelectorGUI.java
@@ -1,17 +1,20 @@
 package com.ds4h.view;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.imageio.ImageIO;
-import java.awt.Panel;
-import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class CornerSelectorGUI extends Panel {
+public class CornerSelectorGUI extends Panel implements MouseListener {
 
     private Image image;
+    private List<Integer[]> points = new ArrayList<>();
 
     public CornerSelectorGUI() {
+        addMouseListener(this);
     }
 
     public void loadImages(File[] files) {
@@ -28,6 +31,21 @@ public class CornerSelectorGUI extends Panel {
 
     public void paint(Graphics g) {
         g.drawImage(image, 0, 0, this);
+        g.setColor(Color.RED);
+        for (Integer[] point : points) {
+            g.fillOval(point[0]-5, point[1]-5, 10, 10);
+        }
     }
 
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        points.add(new Integer[]{x, y});
+        repaint();
+    }
+
+    public void mousePressed(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
 }
