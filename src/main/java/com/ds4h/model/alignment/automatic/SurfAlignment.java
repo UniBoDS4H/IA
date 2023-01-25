986db61523e87826e2ef2b3c9833a37b58e91981
Matteo <matteo.iorio01@gmail.com>
2023-01-25 10:15:13 +0100

    implemented the SURF algorithm

--- src/main/java/com/ds4h/model/alignment/automatic/SurfAlignment.java
+++ src/main/java/com/ds4h/model/alignment/automatic/SurfAlignment.java
@@ -1,82 +1,75 @@
 package com.ds4h.model.alignment.automatic;
 
-import ij.ImagePlus;
-import org.bytedeco.opencv.opencv_core.DMatchVector;
 import org.bytedeco.opencv.opencv_core.KeyPointVector;
-import org.bytedeco.opencv.opencv_core.Point2fVector;
-import org.bytedeco.opencv.opencv_features2d.BFMatcher;
-import org.bytedeco.opencv.opencv_xfeatures2d.SURF;
-import org.opencv.core.DMatch;
-import org.opencv.core.KeyPoint;
-import org.opencv.core.Mat;
-import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.*;
+import org.opencv.features2d.*;
+import org.opencv.imgcodecs.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.xfeatures2d.SURF;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.bytedeco.opencv.global.opencv_calib3d.findHomography;
-import static org.bytedeco.opencv.global.opencv_core.NORM_L2;
-import static org.opencv.calib3d.Calib3d.RANSAC;
-import static org.opencv.imgcodecs.Imgcodecs.imread;
-import static org.opencv.imgproc.Imgproc.*;
-
 public class SurfAlignment {
-    public static Mat align(Mat img1, Mat img2){
+    public static Mat align(){
+        //Read the two images you want to align using the Imgcodecs class:
+        final Mat image1 = Imgcodecs.imread("image1.jpg", Imgcodecs.IMREAD_GRAYSCALE);
+        final Mat image2 = Imgcodecs.imread("image2.jpg", Imgcodecs.IMREAD_GRAYSCALE);
 
-        Mat img1Gray = new Mat();
-        Mat img2Gray = new Mat();
-        cvtColor(img1, img1Gray, COLOR_RGB2GRAY);
-        cvtColor(img2, img2Gray, COLOR_RGB2GRAY);
 
         // Detect keypoints and compute descriptors using the SURF algorithm
-        SURF detector = SURF.create();
-        KeyPointVector keypoints1 = new KeyPointVector();
-        KeyPointVector keypoints2 = new KeyPointVector();
-        Mat descriptors1 = new Mat();
-        Mat descriptors2 = new Mat();
-        detector.detectAndCompute(img1Gray, new Mat(), keypoints1, descriptors1);
-        detector.detectAndCompute(img2Gray, new Mat(), keypoints2, descriptors2);
+        final SURF detector = SURF.create();
 
-        // Match the descriptors using the Brute-Force matcher
-        BFMatcher matcher = BFMatcher.create(NORM_L2);
-        DMatchVector matches = new DMatchVector();
-        matcher.match(descriptors1, descriptors2, matches);
+        // Detect the keypoints and compute the descriptors for both images:
+        final MatOfKeyPoint keypoints1 = new MatOfKeyPoint(); // Matrix where are stored all the key points
+        final Mat descriptors1 = new Mat();
+        detector.detectAndCompute(image1 , new Mat(), keypoints1, descriptors1); // Detect and save the keypoints
 
-        // Find the best matches
-        float max_dist = 0;
-        float min_dist = 100;
-        for (int i = 0; i < descriptors1.rows(); i++) {
-            float dist = matches.get(i).distance();
-            if (dist < min_dist)
-                min_dist = dist;
-            if (dist > max_dist)
-                max_dist = dist;
-        }
-        DMatchVector good_matches = new DMatchVector();
-        for (int i = 0; i < descriptors1.rows(); i++) {
-            if (matches.get(i).distance() <= Math.max(2 * min_dist, 0.02))
-                good_matches.push_back(matches.get(i));
-        }
-        Point2fVector srcPoints = new Point2fVector();
-        Point2fVector dstPoints = new Point2fVector();
-        for (int i = 0; i < good_matches.size(); i++) {
-            DMatch match = good_matches.get(i);
-            KeyPoint kp1 = keypoints1.get(match.queryIdx());
-            KeyPoint kp2 = keypoints2.get(match.trainIdx());
-            srcPoints.push_back(kp1.pt());
-            dstPoints.push_back(kp2.pt());
+        // Detect key points for the second image
+        final MatOfKeyPoint keypoints2 = new MatOfKeyPoint(); //  Matrix where are stored all the key points
+        final Mat descriptors2 = new Mat();
+        detector.detectAndCompute(image2, new Mat(), keypoints2, descriptors2); // Detect and save the keypoints
+
+
+        // Use the BFMatcher class to match the descriptors, BRUTE FORCE APPROACH:
+        final BFMatcher matcher = BFMatcher.create();
+        final MatOfDMatch matches = new MatOfDMatch();
+        matcher.match(descriptors1, descriptors2, matches); // save all the matches from image1 and image2
+
+        final MatOfDMatch matches_ = new MatOfDMatch();
+        matches.convertTo(matches_, CvType.CV_32F);  // changed the datatype of the matrix from 8 bit to 32 bit floating point
+        // convert the matrix of matches in to a list of DMatches, which represent the match between keypoints.
+        final List<DMatch> matchesList = matches.toList();
+
+        // convert the matrices of keypoints in to list of keypoints, which represent the list of keypoints in the two images
+        final List<KeyPoint> keypoints1List = keypoints1.toList();
+        final List<KeyPoint> keypoints2List = keypoints2.toList();
+
+
+        final List<Point> points1 = new ArrayList<>();
+        /* the loop is used to iterate through the matches list, and for each match , it adds the corresponding point from the
+           first image to the "points1", and the corresponding point from the second image to the "points2" list.
+        * */
+        for(int i = 0;i<matchesList.size(); i++){
+            points1.add(keypoints1List.get(matchesList.get(i).queryIdx).pt);
         }
 
-        // Compute the homography matrix using RANSAC
-        Mat mask = new Mat();
-        Mat H = findHomography(srcPoints, dstPoints, RANSAC, 3, mask);
+        final List<Point> points2 = new ArrayList<>();
+        for(int i = 0;i<matchesList.size(); i++){
+            points2.add(keypoints2List.get(matchesList.get(i).trainIdx).pt);
+        }
 
-        // Warp the second image using the homography matrix
-        Mat img2Warped = new Mat();
-        warpPerspective(img2, img2Warped, H, img1.size());
+        final MatOfPoint2f points1_ = new MatOfPoint2f();
+        points1_.fromList(points1);
+        final MatOfPoint2f points2_ = new MatOfPoint2f();
+        points2_.fromList(points2);
 
-        return img2Warped;
+        Mat H = Calib3d.findHomography(points1_, points2_, Calib3d.RANSAC, 5);
 
+        Mat alignedImage1 = new Mat();
+        Imgproc.warpPerspective(image1, alignedImage1, H, image2.size());
 
-         return null;
+        return alignedImage1;
     }
 }
