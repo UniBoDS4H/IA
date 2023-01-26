1a4095410cbe64a8fba42ba55e65d828bfaa54de
Matteo <matteo.iorio01@gmail.com>
2023-01-26 11:05:00 +0100

    Added more parameters inside the BunwarpJ GUI, created BunwarpJMode Enum, BunwarpJMinScale Enum and BunwarpJMaxScale Enum

---
+++ src/main/java/com/ds4h/view/bunwarpjGUI/BunwarpJMode.java
@@ -0,0 +1,23 @@
+package com.ds4h.view.bunwarpjGUI;
+
+public enum BunwarpJMode {
+
+    FAST_MODE("FAST", 0),
+    ACCURATE("ACCURATE", 1),
+    MONO("MONO", 2);
+
+    private final String mode;
+    private final int valMode;
+    private BunwarpJMode(final String mode, final int valMode){
+        this.mode = mode;
+        this.valMode = valMode;
+    }
+
+    public int getValue(){
+        return this.valMode;
+    }
+
+    public String toString(){
+        return this.mode;
+    }
+}
--- src/main/java/com/ds4h/view/bunwarpjGUI/BunwarpjGUI.java
+++ src/main/java/com/ds4h/view/bunwarpjGUI/BunwarpjGUI.java
@@ -7,12 +7,17 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-
 public class BunwarpjGUI extends Frame implements StandardGUI {
 
-    //private final JSlider slider;
-    //private final JComboBox<> modeMenu, initial, final;
-    private final JPanel divPanel,
+    private final JSlider slider;
+    private final JComboBox<BunwarpJMode> modeMenu;
+    private final JComboBox<BunwarpJMinScale> initialDef;
+    private final JComboBox<BunwarpJMaxScale>finalDef;
+    private final JPanel modmenuPanel,
+            sliderPanel,
+            initialPanel,
+            finalPanel,
+            divPanel,
             curlPanel,
             landmarkPanel,
             imagePanel,
@@ -25,15 +30,20 @@ public class BunwarpjGUI extends Frame implements StandardGUI {
             imageWeight,
             consistencyWeight,
             thresholdWeight;
+    private BunwarpJMode modeInput;
+    private BunwarpJMinScale minScale;
+    private BunwarpJMaxScale maxScale;
     private final JButton buttonSave, buttonCancel;
     private final GridBagConstraints constraints;
+
+    private int sampleFactor;
 
     private final static double MIN_ZERO = 0.0,
             MIN_ZERO_ONE = 0.01,
             MIN_ONE = 0.1,
             MIN_TEN = 10.0,
-            MIN_SCREEN_MULT = 0.25,
-            MAX_SCREEN_MULT = 0.35;
+            WIDTH_SCREEN_MULT = 0.25,
+            HEIGHT_SCREEN_MULT = 0.50;
 
     private double parDivWeigth = MIN_ZERO,
         parCurlWeigth = MIN_ZERO,
@@ -51,12 +61,20 @@ public class BunwarpjGUI extends Frame implements StandardGUI {
         Dimension screenSize = toolkit.getScreenSize();
         setLayout(new GridBagLayout());
 
+        this.modeInput = BunwarpJMode.FAST_MODE;
+        this.minScale = BunwarpJMinScale.VERY_COARSE;
+        this.maxScale = BunwarpJMaxScale.VERY_COARSE;
+
         // Set the Frame size
-        int width = (int) (screenSize.width * BunwarpjGUI.MIN_SCREEN_MULT);
-        int height = (int) (screenSize.height * BunwarpjGUI.MAX_SCREEN_MULT);
+        int width = (int) (screenSize.width * BunwarpjGUI.WIDTH_SCREEN_MULT);
+        int height = (int) (screenSize.height * BunwarpjGUI.HEIGHT_SCREEN_MULT);
         setSize(width, height);
 
         // Input field Panel
+        this.modmenuPanel = new JPanel();
+        this.sliderPanel = new JPanel();
+        this.initialPanel = new JPanel();
+        this.finalPanel = new JPanel();
         this.divPanel = new JPanel();
         this.curlPanel = new JPanel();
         this.landmarkPanel = new JPanel();
@@ -69,7 +87,10 @@ public class BunwarpjGUI extends Frame implements StandardGUI {
         this.constraints.insets = new Insets(0, 0, 5, 5);
         this.constraints.anchor = GridBagConstraints.WEST;
 
-
+        this.slider = new JSlider(0, 7);
+        this.modeMenu = new JComboBox<>();
+        this.initialDef = new JComboBox<>();
+        this.finalDef = new JComboBox<>();
         // Init the input fields
         this.divWeight = new JFormattedTextField(String.valueOf(BunwarpjGUI.MIN_ZERO));
         this.curlWeight = new JFormattedTextField(String.valueOf(BunwarpjGUI.MIN_ZERO));
@@ -79,6 +100,7 @@ public class BunwarpjGUI extends Frame implements StandardGUI {
         this.thresholdWeight = new JFormattedTextField(String.valueOf(BunwarpjGUI.MIN_ZERO_ONE));
 
         // Init the buttons
+        this.sampleFactor = this.slider.getValue();
         this.buttonSave = new JButton("Save");
         this.buttonCancel = new JButton("Cancel");
         this.addComponents();
@@ -87,6 +109,10 @@ public class BunwarpjGUI extends Frame implements StandardGUI {
 
     @Override
     public void showDialog() {
+        this.modeMenu.setSelectedItem(this.modeInput);
+        this.initialDef.setSelectedItem(this.minScale);
+        this.finalDef.setSelectedItem(this.maxScale);
+        this.slider.setValue(this.sampleFactor);
         this.divWeight.setValue(String.valueOf(this.parDivWeigth));
         this.curlWeight.setValue(String.valueOf(this.parCurlWeigth));
         this.landmarkWeight.setValue(String.valueOf(this.parLandmarkWeigth));
@@ -105,6 +131,10 @@ public class BunwarpjGUI extends Frame implements StandardGUI {
 
         this.buttonSave.addActionListener(event -> {
             try {
+                this.modeInput = this.modeMenu.getItemAt(this.modeMenu.getSelectedIndex());
+                this.minScale = this.initialDef.getItemAt(this.initialDef.getSelectedIndex());
+                this.maxScale = this.finalDef.getItemAt(this.finalDef.getSelectedIndex());
+                this.sampleFactor = this.slider.getValue();
                 this.parDivWeigth = this.checkInput(this.divWeight) ? Double.parseDouble(this.divWeight.getText()) : BunwarpjGUI.MIN_ZERO;
                 this.parCurlWeigth = this.checkInput(this.curlWeight) ? Double.parseDouble(this.curlWeight.getText()) : BunwarpjGUI.MIN_ZERO;
                 this.parLandmarkWeigth = this.checkInput(this.landmarkWeight) ? Double.parseDouble(this.landmarkWeight.getText()) : BunwarpjGUI.MIN_ZERO;
@@ -133,7 +163,13 @@ public class BunwarpjGUI extends Frame implements StandardGUI {
      */
     @Override
     public void addComponents() {
-
+        this.fillCombo(BunwarpJMode.values());
+        this.fillCombo(BunwarpJMinScale.values());
+        this.fillCombo(BunwarpJMaxScale.values());
+        this.addElement(new JLabel("Mode : "), this.modmenuPanel, this.modeMenu);
+        this.addElement(new JLabel("Initial_Deformation : "), this.initialPanel, this.initialDef);
+        this.addElement(new JLabel("Final_Deformation : "), this.finalPanel, this.finalDef);
+        this.addElement(new JLabel(("Image Sample Factor : ")), this.sliderPanel, this.slider);
         this.addElement(new JLabel("Divergence Weight :"), this.divPanel, this.divWeight);
         this.addElement(new JLabel("Curl Weight :"), this.curlPanel, this.curlWeight);
         this.addElement(new JLabel("Landmark Weight :"), this.landmarkPanel, this.landmarkWeight);
@@ -146,6 +182,26 @@ public class BunwarpjGUI extends Frame implements StandardGUI {
         add(this.buttonPanel, this.constraints);
 
     }
+
+    private void fillCombo(final BunwarpJMode[] values){
+        for(BunwarpJMode mode : values){
+            this.modeMenu.addItem(mode);
+        }
+    }
+
+    private void fillCombo(final BunwarpJMinScale[] values){
+        for(BunwarpJMinScale mode : values){
+            this.initialDef.addItem(mode);
+        }
+    }
+
+    private void fillCombo(final BunwarpJMaxScale[] values){
+        for(BunwarpJMaxScale mode : values){
+            this.finalDef.addItem(mode);
+        }
+    }
+
+
 
     /**
      * Add a single element inside the GUI.
@@ -162,7 +218,34 @@ public class BunwarpjGUI extends Frame implements StandardGUI {
         this.constraints.gridy++;
         add(panel, this.constraints);
     }
+    private void addElement(final JLabel label, final JPanel panel, final JSlider slider){
+        label.setPreferredSize(new Dimension(BunwarpjGUI.WIDTH, BunwarpjGUI.HEIGHT));
+        slider.setMajorTickSpacing(7);
+        slider.setMinorTickSpacing(1);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        panel.add(label);
+        panel.add(slider);
+        this.constraints.gridx = 0;
+        this.constraints.gridy++;
+        add(panel, this.constraints);
+    }
 
+    /**
+     * Add a single element inside the GUI. In particular the element is a JComboBox
+     * @param label : The label where we show which parameter are we setting
+     * @param panel : The panel where we store the label and the field
+     * @param combo : Combobox where a stored the infos for BunwarpJ settings (Mode, MinScaleDeformation and MaxScaleDeformation)
+     */
+    private void addElement(final JLabel label, final JPanel panel, final JComboBox<? extends Enum> combo){
+        label.setPreferredSize(new Dimension(BunwarpjGUI.WIDTH, BunwarpjGUI.HEIGHT));
+        combo.setPreferredSize(new Dimension(BunwarpjGUI.WIDTH, BunwarpjGUI.HEIGHT));
+        panel.add(label);
+        panel.add(combo);
+        this.constraints.gridx = 0;
+        this.constraints.gridy++;
+        add(panel, this.constraints);
+    }
     private boolean checkInput(final JFormattedTextField field){
         try {
             if(!field.getText().isEmpty()){
