59f7dbdd5f3f7ae502aabc5b0114c6af799b174e
Matteo <matteo.iorio01@gmail.com>
2023-02-01 12:06:14 +0100

    created the ConfigureImagesGUI for the OveralpGUI

--- src/main/java/com/ds4h/view/overlapImages/OverlapImagesGUI.java
+++ src/main/java/com/ds4h/view/overlapImages/OverlapImagesGUI.java
@@ -1,6 +1,7 @@
 package com.ds4h.view.overlapImages;
 
 import com.ds4h.controller.AlignmentController.ManualAlignmentController.ManualAlignmentController;
+import com.ds4h.view.configureImageGUI.ConfigureImagesGUI;
 import com.ds4h.view.displayInfo.DisplayInfo;
 import com.ds4h.view.standardGUI.StandardGUI;
 import ij.ImagePlus;
@@ -13,20 +14,19 @@ import java.util.List;
 public class OverlapImagesGUI extends JFrame implements StandardGUI {
     private final JLayeredPane panel;
     private final List<ImagePlus> images;
+    private final JMenuBar menu;
+    private final JMenu settingsMenu;
+    private final JMenuItem settingsImages;
     public OverlapImagesGUI(final List<ImagePlus> images){
         this.setTitle("Final Result");
         this.images = images;
-        int layer = 0;
         this.panel = new JLayeredPane();
+        this.menu = new JMenuBar();
+        this.settingsMenu = new JMenu("Settings");
+        this.settingsImages = new JMenuItem("configure images");
         this.addComponents();
         // TODO : ADD THE POSSIBILITY TO CHANGE FOR EACH IMAGE THE OPACITY AND THE RGB COLOR
-        for(ImagePlus image : this.images){
-            final ImagePanel imagePanel = new ImagePanel(image.getImage());
-            imagePanel.setBounds(new Rectangle(image.getWidth(), image.getHeight()));
-            imagePanel.setOpaque(false);
-            imagePanel.setOpacity(0.2f);
-            this.panel.add(imagePanel, new Integer(layer++));
-        }
+
 
         this.addListeners();
         this.showDialog();
@@ -39,13 +39,32 @@ public class OverlapImagesGUI extends JFrame implements StandardGUI {
 
     @Override
     public void addListeners() {
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.settingsImages.addActionListener(event -> {
+            new ConfigureImagesGUI();
+        });
     }
 
     @Override
     public void addComponents() {
+        this.overlapImages();
         this.add(this.panel);
+        this.menu.add(this.settingsMenu);
+        this.settingsMenu.add(this.settingsImages);
+        this.setJMenuBar(this.menu);
         this.setSize(new Dimension(this.images.get(0).getWidth(), this.images.get(0).getHeight()));
+    }
+
+    private void overlapImages(){
+        int layer = 0;
+        for(ImagePlus image : this.images){
+            final ImagePanel imagePanel = new ImagePanel(image.getImage());
+            imagePanel.setBounds(new Rectangle(image.getWidth(), image.getHeight()));
+            imagePanel.setOpaque(false);
+            imagePanel.setOpacity(0.2f);
+            this.panel.add(imagePanel, new Integer(layer++));
+        }
     }
     private class ImagePanel extends JPanel {
         private final Image image;
