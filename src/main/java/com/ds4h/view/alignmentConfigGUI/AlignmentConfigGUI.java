31a3e193f6ec5321150ae92591c390718be0e740
Matteo <matteo.iorio01@gmail.com>
2023-02-22 10:42:43 +0100

    created the GUI for the selection of the manual algorithm

--- src/main/java/com/ds4h/view/alignmentConfigGUI/AlignmentConfigGUI.java
+++ src/main/java/com/ds4h/view/alignmentConfigGUI/AlignmentConfigGUI.java
@@ -1,15 +1,26 @@
 package com.ds4h.view.alignmentConfigGUI;
 
+import com.ds4h.view.bunwarpjGUI.BunwarpjGUI;
 import com.ds4h.view.standardGUI.StandardGUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AlignmentConfigGUI extends JFrame implements StandardGUI {
     private final JComboBox<AlignmentAlgorithm> algorithm;
+    private final JButton saveButton;
+    private final JLabel text;
+    private final GridBagConstraints constraints;
     private AlignmentAlgorithm selectedValue;
     public AlignmentConfigGUI(){
         this.setTitle("Pick manual alignment algorithm");
+        this.setLayout(new GridBagLayout());
         this.algorithm = new JComboBox<>();
+        this.saveButton = new JButton("Save");
+        this.text = new JLabel();
+        this.constraints = new GridBagConstraints();
+        this.constraints.insets = new Insets(0, 0, 5, 5);
+        this.constraints.anchor = GridBagConstraints.WEST;
         this.addComponents();
         this.addListeners();
     }
@@ -25,6 +36,10 @@ public class AlignmentConfigGUI extends JFrame implements StandardGUI {
         this.algorithm.addActionListener(event -> {
             this.selectedValue = (AlignmentAlgorithm) this.algorithm.getSelectedItem();
         });
+        this.saveButton.addActionListener(event -> {
+            this.selectedValue = (AlignmentAlgorithm) this.algorithm.getSelectedItem();
+            this.dispose();
+        });
     }
 
     public AlignmentAlgorithm getSelectedAlgorithm(){
@@ -34,8 +49,26 @@ public class AlignmentConfigGUI extends JFrame implements StandardGUI {
     @Override
     public void addComponents() {
         this.populateCombo();
-        this.add(this.algorithm);
+        this.setSize();
+        this.addElement(new JLabel("Pick the algorithm: "), new JPanel(), this.algorithm);
+        this.addElement(new JLabel("Info about: "), new JPanel(), this.text);
+        this.constraints.gridy++;
+        this.add(this.saveButton, this.constraints);
     }
+
+    private void setSize(){
+        this.setSize(new Dimension(400, 400));
+    }
+
+    private void addElement(final JLabel label, final JPanel panel, final JComponent component){
+        label.setPreferredSize(new Dimension(200, 50));
+        panel.add(label);
+        panel.add(component);
+        this.constraints.gridx = 0;
+        this.constraints.gridy++;
+        this.add(panel, this.constraints);
+    }
+
     private void populateCombo(){
         this.selectedValue = AlignmentAlgorithm.TRANSLATIVE;
         for(AlignmentAlgorithm algorithm : AlignmentAlgorithm.values()){
