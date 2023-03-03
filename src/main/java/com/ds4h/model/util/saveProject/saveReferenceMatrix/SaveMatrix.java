604442fd05a6715df3c15443272e3c2b2a6d2415
Matteo <matteo.iorio01@gmail.com>
2023-03-03 12:13:17 +0100

    saved reference matrix inside txt files

--- src/main/java/com/ds4h/model/util/saveProject/saveReferenceMatrix/SaveMatrix.java
+++ src/main/java/com/ds4h/model/util/saveProject/saveReferenceMatrix/SaveMatrix.java
@@ -5,6 +5,9 @@ import com.ds4h.model.util.json.jsonSerializer.JSONSerializer;
 import org.opencv.core.Mat;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,6 +22,33 @@ public class SaveMatrix {
     }
 
     public static void saveMatrix(final List<AlignedImage> images, final String path){
-        JSONSerializer.createJSON(images, path);
+        //JSONSerializer.createJSON(images, path);
+        StringBuilder content = new StringBuilder();
+        for(final AlignedImage alignedImage : images){
+            if(alignedImage.getRegistrationMatrix().isPresent()) {
+                content.append("REFERENCE: ").append(alignedImage.getAlignedImage().getTitle()).append("\n");
+                content.append("MATRIX: ").append("\n");
+                final Mat matrix = alignedImage.getRegistrationMatrix().get();
+                final StringBuilder stringBuilder = new StringBuilder();
+                for (int row = 0; row < matrix.rows(); row++) {
+                    for (int col = 0; col < matrix.cols(); col++) {
+                        stringBuilder.append(matrix.get(row, col)[0]).append(" ");
+                    }
+                    stringBuilder.append("\n");
+                }
+                content.append(stringBuilder.toString());
+            }
+            content.append("\n \n \n");
+        }
+        SaveMatrix.save(path, content.toString());
+    }
+
+    private static void save(final String path, final String content){
+        try (FileWriter file = new FileWriter(path+"/"+"DS4H_reference_matrix.txt")) {
+            file.write(content.toString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
