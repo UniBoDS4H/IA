1f9be92c6f534249e7e31aa9c8b4f02fcb8d5134
Matteo <matteo.iorio01@gmail.com>
2023-03-09 09:23:52 +0100

    added the KAZE detector and the BRISK detector

---
+++ src/main/java/com/ds4h/model/alignment/automatic/pointDetector/briskDetector/BRISKDetector.java
@@ -0,0 +1,14 @@
+package com.ds4h.model.alignment.automatic.pointDetector.briskDetector;
+
+import com.ds4h.model.alignment.automatic.pointDetector.PointDetector;
+import com.ds4h.model.imagePoints.ImagePoints;
+import org.opencv.features2d.BRISK;
+import org.opencv.xfeatures2d.SIFT;
+
+public class BRISKDetector extends PointDetector {
+
+    @Override
+    public void detectPoint(ImagePoints targetImage, ImagePoints imagePoint) {
+
+    }
+}
--- src/main/java/com/ds4h/model/alignment/automatic/pointDetector/kazeDetector/KAZEDetector.java
+++ src/main/java/com/ds4h/model/alignment/automatic/pointDetector/kazeDetector/KAZEDetector.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KAZEDetector extends PointDetector {
-    private final AKAZE detector = AKAZE.create();
+    private final KAZE detector = KAZE.create();
     private final BFMatcher matcher = BFMatcher.create();
     public KAZEDetector(){
         super();
