a99503c0fcf5ab6f71d1e1709c3ad601e0c773d6
Matteo <matteo.iorio01@gmail.com>
2023-04-11 17:32:28 +0200

    removed warnings

--- src/main/java/com/ds4h/model/util/directoryManager/directoryDeleter/DirectoryDeleter.java
+++ src/main/java/com/ds4h/model/util/directoryManager/directoryDeleter/DirectoryDeleter.java
@@ -5,31 +5,42 @@ import ij.IJ;
 import java.io.File;
 import java.util.Objects;
 
+/**
+ *
+ */
 public class DirectoryDeleter {
     private final static String TEMPORARY_PATH = System.getProperty("java.io.tmpdir");
-
     private DirectoryDeleter(){
 
     }
 
+    /**
+     *
+     * @param path a
+     * @param directoryName b
+     */
     public static void delete(final String path, final String directoryName){
         final File directory = new File(path);
-        try {
-            if(directory.isDirectory()) {
-                for (final File file : Objects.requireNonNull(directory.listFiles())) {
-                    if (file.isDirectory() && file.getName().contains(directoryName)) {
-                        for (final File image : Objects.requireNonNull(file.listFiles())) {
-                            image.delete();
+        if(directory.isDirectory()) {
+            for (final File file : Objects.requireNonNull(directory.listFiles())) {
+                if (file.isDirectory() && file.getName().contains(directoryName)) {
+                    for (final File image : Objects.requireNonNull(file.listFiles())) {
+                        if(image.delete()){
+                            IJ.log("Directory deleted.");
                         }
-                        file.delete();
+                    }
+                    if(file.delete()){
+                        IJ.log("Directory deleted.");
                     }
                 }
             }
-        }catch(final Exception exception){
-            IJ.showMessage(exception.getMessage());
         }
     }
 
+    /**
+     *
+     * @param directoryName a
+     */
     public static void deleteTMP(final String directoryName){
         DirectoryDeleter.delete(TEMPORARY_PATH, directoryName);
     }
