98fb03003398208f9f9ea5b8edcdce19d234e70e
Matteo <matteo.iorio01@gmail.com>
2023-04-11 15:33:37 +0200

    removed warnings from save

--- src/main/java/com/ds4h/model/util/saveProject/SaveImages.java
+++ src/main/java/com/ds4h/model/util/saveProject/SaveImages.java
@@ -5,10 +5,6 @@ import com.ds4h.model.util.directoryManager.directoryCreator.DirectoryCreator;
 import com.ds4h.model.util.saveProject.saveReferenceMatrix.SaveMatrix;
 import ij.IJ;
 import ij.ImagePlus;
-
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -18,8 +14,6 @@ import java.util.stream.Collectors;
  */
 public class SaveImages {
     private final static String DIRECTORY = "DS4H_AlignedImages";
-    public final static String TMP_DIRECTORY = "TMP_DS4H_AlignedImages";
-    private final static String TEMPORARY_PATH = System.getProperty("java.io.tmpdir");
 
     private SaveImages(){
 
@@ -29,9 +23,8 @@ public class SaveImages {
      *
      * @param images a
      * @param path b
-     * @throws IOException c
      */
-    public static void saveImages(final List<AlignedImage> images, final String path) throws IOException {
+    public static void saveImages(final List<AlignedImage> images, final String path) {
         final String dir = DirectoryCreator.createDirectory(path, DIRECTORY);
         if(!dir.isEmpty()){
             SaveImages.save(images.stream().map(AlignedImage::getAlignedImage).collect(Collectors.toList()), path+"/"+dir);
@@ -44,43 +37,11 @@ public class SaveImages {
     /**
      *
      * @param images a
-     * @return b
-     */
-    public static String saveTMPImages(final List<ImagePlus> images){
-        final String dir = DirectoryCreator.createTemporaryDirectory(TMP_DIRECTORY);
-        if(!dir.isEmpty()){
-            SaveImages.save(images, TEMPORARY_PATH+ "/" + dir);
-        }
-        return TEMPORARY_PATH + "/" + dir;
-    }
-
-    /**
-     *
-     * @param image a
-     * @return b
-     */
-    public static String saveTMPImage(final ImagePlus image){
-        final String dir = DirectoryCreator.createTemporaryDirectory(TMP_DIRECTORY);
-        if(!dir.isEmpty()){
-            SaveImages.saveOne(image, TEMPORARY_PATH+ "/" + dir);
-        }
-        return TEMPORARY_PATH + "/" + dir;
-    }
-
-    private static void saveOne(final ImagePlus image, final String path){
-        IJ.save(image, path+"/"+image.getTitle());
-    }
-
-    /**
-     *
-     * @param images a
      * @param path b
      */
     public static void save(final List<ImagePlus> images, final String path){
         //TODO: The parallel can cause out of memory exception.
-        images.parallelStream().forEach(image -> {
-            IJ.save(image, path+"/"+image.getTitle());
-        });
+        images.parallelStream().forEach(image -> IJ.save(image, path+"/"+image.getTitle()));
     }
 
     /**
@@ -92,14 +53,5 @@ public class SaveImages {
         if(!path.isEmpty() && Objects.nonNull(image)) {
             IJ.save(image, path + "/" + image.getTitle());
         }
-    }
-
-    private static String getFormat(final ImagePlus image){
-        try {
-            return ImageIO.getImageReaders(ImageIO.createImageInputStream(image.getImage())).next().getFormatName();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "";
     }
 }
