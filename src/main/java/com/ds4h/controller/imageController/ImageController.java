6c2199a66490f2e0048cad835f9438153290e6ed
Matteo <matteo.iorio01@gmail.com>
2023-04-13 09:30:09 +0200

    Implemented BUnwarpJ

--- src/main/java/com/ds4h/controller/imageController/ImageController.java
+++ src/main/java/com/ds4h/controller/imageController/ImageController.java
@@ -3,8 +3,15 @@ package com.ds4h.controller.imageController;
 import com.ds4h.controller.alignmentController.AlignmentControllerInterface;
 import com.ds4h.controller.bunwarpJController.BunwarpJController;
 import com.ds4h.model.alignedImage.AlignedImage;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.process.ByteProcessor;
+import ij.process.ImageConverter;
 
+import java.awt.image.ColorModel;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
@@ -21,6 +28,41 @@ public class ImageController {
     public ImageController(final AlignmentControllerInterface alignmentControllerInterface, final BunwarpJController bunwarpJController){
         this.alignmentControllerInterface = alignmentControllerInterface;
         this.bunwarpJController = bunwarpJController;
+    }
+
+    public ImagePlus getAlignedImagesAsStack() throws RuntimeException{
+        if(!this.getAlignedImages().isEmpty()){
+            final ImageStack stack = new ImageStack(this.getAlignedImages().get(0).getAlignedImage().getWidth(),
+                    this.getAlignedImages().get(0).getAlignedImage().getHeight(), ColorModel.getRGBdefault());
+            System.gc();
+            final List<AlignedImage> images = this.getAlignedImages();
+            //final LUT[] luts = new LUT[images.size()];
+            //int index = 0;
+            final int bitDepth = images.stream()
+                    .min(Comparator.comparingInt(img -> img.getAlignedImage().getBitDepth())).get().getAlignedImage().getBitDepth();
+            IJ.log("[ALIGNED STACK] Bit Depth: " + bitDepth);
+            for (final AlignedImage image : images) {
+                if(!(image.getAlignedImage().getProcessor() instanceof ByteProcessor)) {
+                    final ImageConverter imageConverter = new ImageConverter(image.getAlignedImage());
+                    imageConverter.convertToGray8();
+                    IJ.log("[STACK] " + (image.getAlignedImage().getProcessor() instanceof ByteProcessor));
+                }
+                //luts[index] = image.getAlignedImage().getProcessor().getLut();
+                IJ.log("[NAME] " + image.getName());
+                stack.addSlice(image.getName(), image.getAlignedImage().getProcessor());
+                //index++;
+            }
+            try {
+                //final CompositeImage composite = new CompositeImage(new ImagePlus("Aligned Stack", stack));
+                //composite.setLuts(luts);
+                return new ImagePlus("Aligned Stack", stack);
+            }catch (Exception e){
+                throw new RuntimeException("Something went wrong with the creation of the stack.");
+            }
+        }
+        throw new RuntimeException("The detection has failed, the number of points found can not be used with the selected \"Algorithm\".\n" +
+                "Please consider to expand the memory (by going to Edit > Options > Memory & Threads)\n" +
+                "increase the Threshold Factor and change the \"Algorithm\".");
     }
 
     /**
--- src/main/java/com/ds4h/view/outputGUI/AlignmentOutputGUI.java
+++ src/main/java/com/ds4h/view/outputGUI/AlignmentOutputGUI.java
@@ -1,16 +1,20 @@
 package com.ds4h.view.outputGUI;
 
 import com.ds4h.controller.alignmentController.AlignmentControllerInterface;
+import com.ds4h.controller.alignmentController.ManualAlignmentController.ManualAlignmentController;
 import com.ds4h.controller.bunwarpJController.BunwarpJController;
 import com.ds4h.controller.imageController.ImageController;
 import com.ds4h.controller.pointController.PointController;
 import com.ds4h.view.bunwarpjGUI.BunwarpjGUI;
 import com.ds4h.view.configureImageGUI.ConfigureImagesGUI;
+import com.ds4h.view.loadingGUI.LoadingGUI;
+import com.ds4h.view.loadingGUI.LoadingType;
 import com.ds4h.view.mainGUI.MainMenuGUI;
 import com.ds4h.view.reuseGUI.ReuseGUI;
 import com.ds4h.view.saveImagesGUI.SaveImagesGUI;
 import com.ds4h.view.standardGUI.StandardCanvas;
 import ij.CompositeImage;
+import ij.IJ;
 import ij.ImagePlus;
 import ij.gui.StackWindow;
 import ij.process.LUT;
@@ -27,6 +31,8 @@ public class AlignmentOutputGUI extends StackWindow {
     private final Menu settings;
     private final Menu reuse;
     private final Menu save;
+    private final Menu elastic;
+    private final MenuItem elasticItem;
     private final MenuItem reuseItem;
     private final MenuItem overlappedItem;
     private final MenuItem saveItem;
@@ -37,17 +43,21 @@ public class AlignmentOutputGUI extends StackWindow {
     private final SaveImagesGUI saveGui;
     private final ImageController controller;
     private final ConfigureImagesGUI configureImagesGUI;
+    private final BunwarpJController bunwarpJController;
+    private final AlignmentControllerInterface alignmentControllerInterface;
     private final PointController pointController;
     private final MainMenuGUI mainGUI;
     private final LUT[] originalLuts;
 
-    public AlignmentOutputGUI(AlignmentControllerInterface alignmentController, BunwarpjGUI settingsBunwarpj, BunwarpJController bunwarpJController, PointController pointController, MainMenuGUI mainMenuGUI) {
+    public AlignmentOutputGUI(final AlignmentControllerInterface alignmentController, final BunwarpjGUI settingsBunwarpj, final BunwarpJController bunwarpJController, final PointController pointController, final MainMenuGUI mainMenuGUI) {
         super(image = alignmentController.getAlignedImagesAsStack(), new StandardCanvas(image));
         this.canvas = (StandardCanvas)this.getCanvas();
         this.removeAll();
         this.setLayout(new BorderLayout());
         this.originalLuts = this.getImagePlus().getLuts();
         this.mainGUI = mainMenuGUI;
+        this.bunwarpJController = bunwarpJController;
+        this.alignmentControllerInterface = alignmentController;
         this.pointController = pointController;
         this.controller = new ImageController(alignmentController, bunwarpJController);
         this.configureImagesGUI = new ConfigureImagesGUI(this);
@@ -59,12 +69,47 @@ public class AlignmentOutputGUI extends StackWindow {
         this.menuBar = new MenuBar();
         this.settingsImages = new MenuItem("Configure images");
         this.settings = new Menu("Settings");
+        this.elastic = new Menu("Elastic");
         this.reuse = new Menu("Reuse");
         this.save = new Menu("Save");
+        this.elasticItem = new MenuItem("Elastic deformation");
         this.reuseItem = new MenuItem("Reuse as source");
-        this.overlappedItem = new MenuItem("View Overlapped");
-        this.carouselItem = new MenuItem("View Carousel");
-        this.saveItem = new MenuItem("Save Project");
+        this.overlappedItem = new MenuItem("View overlapped");
+        this.carouselItem = new MenuItem("View carousel");
+        this.saveItem = new MenuItem("Save project");
+        this.addComponents();
+        this.addListeners();
+        this.createOutputIcons();
+    }
+
+    public AlignmentOutputGUI(final ImagePlus stack, final AlignmentControllerInterface alignmentController,  final BunwarpjGUI settingsBunwarpj, final BunwarpJController bunwarpJController, final PointController pointController, final MainMenuGUI mainMenuGUI) {
+        super(image = stack, new StandardCanvas(image));
+        this.canvas = (StandardCanvas)this.getCanvas();
+        this.removeAll();
+        this.setLayout(new BorderLayout());
+        this.originalLuts = this.getImagePlus().getLuts();
+        this.mainGUI = mainMenuGUI;
+        this.bunwarpJController = bunwarpJController;
+        this.alignmentControllerInterface = alignmentController;
+        this.pointController = pointController;
+        this.controller = new ImageController(alignmentController, bunwarpJController);
+        this.configureImagesGUI = new ConfigureImagesGUI(this);
+        this.algorithm = alignmentController.name();
+        this.bunwarpjGUI = settingsBunwarpj;
+        this.saveGui = new SaveImagesGUI(this.controller);
+        this.panel = new JPanel();
+        this.panel.setLayout(new BorderLayout());
+        this.menuBar = new MenuBar();
+        this.settingsImages = new MenuItem("Configure images");
+        this.settings = new Menu("Settings");
+        this.elastic = new Menu("Elastic");
+        this.reuse = new Menu("Reuse");
+        this.save = new Menu("Save");
+        this.elasticItem = new MenuItem("Elastic deformation");
+        this.reuseItem = new MenuItem("Reuse as source");
+        this.overlappedItem = new MenuItem("View overlapped");
+        this.carouselItem = new MenuItem("View carousel");
+        this.saveItem = new MenuItem("Save project");
         this.addComponents();
         this.addListeners();
         this.createOutputIcons();
@@ -90,10 +135,12 @@ public class AlignmentOutputGUI extends StackWindow {
         this.menuBar.add(this.settings);
         this.menuBar.add(this.save);
         this.menuBar.add(this.reuse);
+        this.menuBar.add(this.elastic);
         this.settings.add(this.settingsImages);
         this.settings.add(this.overlappedItem);
         this.save.add(this.saveItem);
         this.reuse.add(this.reuseItem);
+        this.elastic.add(this.elasticItem);
         this.panel.add(this.canvas, BorderLayout.CENTER);
         this.setMenuBar(this.menuBar);
         this.panel.add(sliceSelector, BorderLayout.PAGE_END);
@@ -126,17 +173,41 @@ public class AlignmentOutputGUI extends StackWindow {
             this.settings.remove(this.overlappedItem);
             this.settings.add(this.carouselItem);
         });
+
         this.carouselItem.addActionListener(event -> {
             this.getImagePlus().setDisplayMode(2);
             this.settings.remove(this.carouselItem);
             this.settings.add(this.overlappedItem);
         });
+
         this.saveItem.addActionListener(event -> {
             this.saveGui.showDialog();
         });
+
         this.reuseItem.addActionListener(event -> {
             final ReuseGUI reuseGUI = new ReuseGUI(this.pointController, this.controller, this.mainGUI, this);
             reuseGUI.showDialog();
+        });
+
+        this.elasticItem.addActionListener(event -> {
+            this.controller.elastic(this.alignmentControllerInterface.getAlignedImages());
+            final Thread pollingElastic = new Thread(() -> {
+                try {
+                    while (this.controller.deformationIsAlive()){
+                        Thread.sleep(1000);
+                    }
+                    if(this.controller.getAlignedImages().size() > 0) {
+                        new AlignmentOutputGUI(this.controller.getAlignedImagesAsStack(), this.alignmentControllerInterface, this.bunwarpjGUI, this.bunwarpJController, this.pointController, this.mainGUI);
+                        this.dispose();
+                    }
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(this,
+                            e.getMessage(),
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            });
+            pollingElastic.start();
         });
     }
 
