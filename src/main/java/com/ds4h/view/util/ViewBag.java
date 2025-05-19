75a5264627d03c2ecaa39398cfd0c979d6e7204a
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-05-19 11:31:53 +0200

    feat: init ViewBag class

---
+++ src/main/java/com/ds4h/view/util/ViewBag.java
@@ -0,0 +1,12 @@
+package com.ds4h.view.util;
+
+import com.ds4h.model.imagePoints.ImagePoints;
+import com.ds4h.view.pointSelectorGUI.PointSelectorCanvas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ViewBag {
+    public static Map<ImagePoints, PointSelectorCanvas> references = new HashMap<>();
+    
+}
