bfef7ac4cb40a203f8acd661018e4dcdb13ee7a8
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-06 09:26:59 +0200

    doc: add javadoc

--- src/main/java/com/ds4h/model/image/PointRepository.java
+++ src/main/java/com/ds4h/model/image/PointRepository.java
@@ -3,6 +3,15 @@ package com.ds4h.model.image;
 import org.opencv.core.Point;
 
 public interface PointRepository {
+    /**
+     * Add a new Point inside the repository.
+     * @param point new detected point.
+     */
     void add(Point point);
+
+    /**
+     * Return an iterable of all the point stored inside the repository.
+     * @return an iterable of points.
+     */
     Iterable<Point> getPoints();
 }
