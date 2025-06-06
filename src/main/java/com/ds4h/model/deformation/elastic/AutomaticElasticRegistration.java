91e9e45d261beddbfcc205cff3f356e70148277e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-06 10:25:00 +0200

    feat: implement ElasticRegistration

--- src/main/java/com/ds4h/model/deformation/elastic/AutomaticElasticRegistration.java
+++ src/main/java/com/ds4h/model/deformation/elastic/AutomaticElasticRegistration.java
@@ -1,4 +1,19 @@
 package com.ds4h.model.deformation.elastic;
 
-public class AutomaticElasticRegistration {
+import com.ds4h.model.alignment.automatic.pointDetector.PointDetector;
+import com.ds4h.model.image.alignedImage.AlignedImage;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.Future;
+
+public class AutomaticElasticRegistration implements ElasticRegistration {
+    final PointDetector detector;
+    public AutomaticElasticRegistration(@NotNull final PointDetector detector) {
+        this.detector = detector;
+    }
+    @NotNull
+    @Override
+    public Future<AlignedImage> transform(@NotNull AlignedImage movingImage, @NotNull AlignedImage targetImage) {
+        return null;
+    }
 }
