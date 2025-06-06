6cf018faf6e50ca8d0354b3cc098251b7bf6ba99
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-06 10:19:30 +0200

    feat: add new interface regarding the elastic registration

---
+++ src/main/java/com/ds4h/model/deformation/elastic/AutomaticElasticRegistration.java
@@ -0,0 +1,4 @@
+package com.ds4h.model.deformation.elastic;
+
+public class AutomaticElasticRegistration {
+}
---
+++ src/main/java/com/ds4h/model/deformation/elastic/ElasticRegistration.java
@@ -0,0 +1,16 @@
+package com.ds4h.model.deformation.elastic;
+
+import com.ds4h.model.image.alignedImage.AlignedImage;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.Future;
+
+public interface ElasticRegistration {
+    /**
+     * Perform an elastic registration using the two input images.
+     * @param movingImage moving image that will be subject to the registration.
+     * @param targetImage target image, this will be fixed.
+     * @return A future containing the transformed image.
+     */
+    Future<AlignedImage> transform(@NotNull AlignedImage movingImage, @NotNull AlignedImage targetImage);
+}
