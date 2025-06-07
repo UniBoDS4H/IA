33bff3ff7dfa6c769cf3121604672d82f2d27be6
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-07 12:12:51 +0200

    feat: init apply elastic registration

--- src/main/java/com/ds4h/model/deformation/elastic/AutomaticElasticRegistration.java
+++ src/main/java/com/ds4h/model/deformation/elastic/AutomaticElasticRegistration.java
@@ -1,8 +1,14 @@
 package com.ds4h.model.deformation.elastic;
 
+import bigwarp.landmarks.LandmarkTableModel;
 import com.ds4h.model.alignment.automatic.pointDetector.PointDetector;
 import com.ds4h.model.image.alignedImage.AlignedImage;
+import ij.ImagePlus;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class AutomaticElasticRegistration implements ElasticRegistration {
@@ -14,6 +20,12 @@ public class AutomaticElasticRegistration implements ElasticRegistration {
     @NotNull
     @Override
     public CompletableFuture<AlignedImage> transform(@NotNull AlignedImage movingImage, @NotNull AlignedImage targetImage) {
-        return CompletableFuture.supplyAsync(() -> null);
+        return CompletableFuture.supplyAsync(() -> {
+
+            return null;
+        });
+    }
+    private List<ImagePlus> applyElasticRegistration(@NotNull final AlignedImage movingImage, @NotNull final AlignedImage targetImage, @NotNull final LandmarkTableModel landmarkTableModel) {
+        return Collections.emptyList();
     }
 }
