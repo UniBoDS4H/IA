1e4251b7e75e94aa6012c67ad9ea965267bd834a
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-07 12:15:11 +0200

    feat: implement apply elastic registration

--- src/main/java/com/ds4h/model/deformation/elastic/AutomaticElasticRegistration.java
+++ src/main/java/com/ds4h/model/deformation/elastic/AutomaticElasticRegistration.java
@@ -1,9 +1,16 @@
 package com.ds4h.model.deformation.elastic;
 
+import bdv.ij.ApplyBigwarpPlugin;
+import bdv.viewer.Interpolation;
+import bigwarp.BigWarpData;
+import bigwarp.BigWarpInit;
 import bigwarp.landmarks.LandmarkTableModel;
+import bigwarp.transforms.BigWarpTransform;
 import com.ds4h.model.alignment.automatic.pointDetector.PointDetector;
 import com.ds4h.model.image.alignedImage.AlignedImage;
 import ij.ImagePlus;
+import net.imglib2.realtransform.BoundingBoxEstimation;
+import net.imglib2.realtransform.InvertibleRealTransform;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
@@ -26,6 +33,27 @@ public class AutomaticElasticRegistration implements ElasticRegistration {
         });
     }
     private List<ImagePlus> applyElasticRegistration(@NotNull final AlignedImage movingImage, @NotNull final AlignedImage targetImage, @NotNull final LandmarkTableModel landmarkTableModel) {
-        return Collections.emptyList();
+        final BigWarpData<?> bigwarpData = BigWarpInit.createBigWarpDataFromImages(movingImage.getAlignedImage(), targetImage.getAlignedImage());
+        bigwarpData.wrapMovingSources();
+        final BoundingBoxEstimation boundingBoxEstimation = new BoundingBoxEstimation(BoundingBoxEstimation.Method.CORNERS);
+        final InvertibleRealTransform invertibleRealTransform = new BigWarpTransform(landmarkTableModel, BigWarpTransform.AFFINE).getTransformation();
+        return ApplyBigwarpPlugin.apply(
+                bigwarpData,
+                landmarkTableModel,
+                invertibleRealTransform,
+                BigWarpTransform.AFFINE, // tform type
+                ApplyBigwarpPlugin.TARGET, // fov option
+                null,
+                boundingBoxEstimation,
+                ApplyBigwarpPlugin.TARGET,
+                null,
+                null,
+                null,
+                Interpolation.NEARESTNEIGHBOR,
+                false, // virtual
+                1, // nThreads
+                true,
+                null, // writeOpts
+                false);
     }
 }
