7843bcd495fdae3ef44cb2d6c214b5e0eb7d75d9
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-07 12:09:51 +0200

    refactor: use CompletableFuture

--- src/main/java/com/ds4h/model/deformation/elastic/AutomaticElasticRegistration.java
+++ src/main/java/com/ds4h/model/deformation/elastic/AutomaticElasticRegistration.java
@@ -3,17 +3,17 @@ package com.ds4h.model.deformation.elastic;
 import com.ds4h.model.alignment.automatic.pointDetector.PointDetector;
 import com.ds4h.model.image.alignedImage.AlignedImage;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class AutomaticElasticRegistration implements ElasticRegistration {
     final PointDetector detector;
     public AutomaticElasticRegistration(@NotNull final PointDetector detector) {
         this.detector = detector;
     }
+
     @NotNull
     @Override
-    public Future<AlignedImage> transform(@NotNull AlignedImage movingImage, @NotNull AlignedImage targetImage) {
-        return null;
+    public CompletableFuture<AlignedImage> transform(@NotNull AlignedImage movingImage, @NotNull AlignedImage targetImage) {
+        return CompletableFuture.supplyAsync(() -> null);
     }
 }
