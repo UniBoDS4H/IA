838fb3a256a2c1dd44b042742bc1a9775f62ceb9
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-09 09:47:41 +0200

    feat: add getImage method and getName method

--- src/main/java/com/ds4h/model/image/AnalyzableImage.java
+++ src/main/java/com/ds4h/model/image/AnalyzableImage.java
@@ -1,4 +1,20 @@
 package com.ds4h.model.image;
 
+import ij.ImagePlus;
+import org.jetbrains.annotations.NotNull;
+
 public interface AnalyzableImage extends PointRepository, DataImage{
+    /**
+     * TODO
+     * @return
+     */
+    @NotNull
+    ImagePlus getImage();
+
+    /**
+     * TODO
+     * @return
+     */
+    @NotNull
+    String getName();
 }
