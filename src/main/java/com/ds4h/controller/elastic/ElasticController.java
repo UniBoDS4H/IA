4137bfdfb87f6cb32c9fca64ecf1fa1c45111166
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-10 09:40:29 +0200

    feat: init elastic controller

--- src/main/java/com/ds4h/controller/elastic/ElasticController.java
+++ src/main/java/com/ds4h/controller/elastic/ElasticController.java
@@ -1,5 +1,24 @@
 package com.ds4h.controller.elastic;
 
+import com.drew.lang.annotations.NotNull;
+import com.ds4h.model.alignment.automatic.pointDetector.PointDetector;
+import com.ds4h.model.image.AnalyzableImage;
+import com.ds4h.model.image.alignedImage.AlignedImage;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 public class ElasticController {
     public ElasticController() {}
+
+    public CompletableFuture<List<AlignedImage>> automaticElasticRegistration(@NotNull final AnalyzableImage targetImage,
+                                                                              @NotNull final List<AnalyzableImage> movingImages,
+                                                                              @NotNull final PointDetector detector) {
+        return null;
+    }
+
+    public CompletableFuture<List<AlignedImage>> manualElasticRegistration(@NotNull final AnalyzableImage targetImage,
+                                                                           @NotNull final List<AnalyzableImage> movingImages) {
+        return null;
+    }
 }
