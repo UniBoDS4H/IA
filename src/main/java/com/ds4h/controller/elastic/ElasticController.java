c1146824e00cd6428a162694bd8293ed8632621b
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-10 09:59:22 +0200

    feat: implement automatic registration

--- src/main/java/com/ds4h/controller/elastic/ElasticController.java
+++ src/main/java/com/ds4h/controller/elastic/ElasticController.java
@@ -1,20 +1,40 @@
 package com.ds4h.controller.elastic;
 
 import com.drew.lang.annotations.NotNull;
+import com.ds4h.model.alignment.automatic.pointDetector.Detectors;
 import com.ds4h.model.alignment.automatic.pointDetector.PointDetector;
+import com.ds4h.model.deformation.elastic.ElasticRegistration;
+import com.ds4h.model.deformation.elastic.ElasticRegistrationImpl;
 import com.ds4h.model.image.AnalyzableImage;
 import com.ds4h.model.image.alignedImage.AlignedImage;
+import com.ds4h.model.pointManager.ImageManager;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ElasticController {
-    public ElasticController() {}
+    final ElasticRegistration elasticRegistration;
+    public ElasticController() {
+        this.elasticRegistration = new ElasticRegistrationImpl();
+    }
 
-    public CompletableFuture<List<AlignedImage>> automaticElasticRegistration(@NotNull final AnalyzableImage targetImage,
-                                                                              @NotNull final List<AnalyzableImage> movingImages,
-                                                                              @NotNull final PointDetector detector) {
-        return null;
+    @NotNull
+    public CompletableFuture<List<AlignedImage>> automaticElasticRegistration(@NotNull final ImageManager imageManager,
+                                                                              @NotNull final Detectors detector) {
+        final List<AlignedImage> outputImages = new LinkedList<>();
+        final PointDetector pointDetector = detector.pointDetector();
+        return CompletableFuture.supplyAsync(() -> {
+            imageManager.getTargetImage().ifPresent(targetImage -> {
+                imageManager.getImagesToAlign().forEach(image -> {
+                    pointDetector.detectPoint(targetImage, image);
+                    this.elasticRegistration.transformImage(targetImage, image)
+                            .whenComplete((transformedImage, e) -> outputImages.add(transformedImage));
+                    targetImage.clear();
+                });
+            });
+           return outputImages;
+        });
     }
 
     public CompletableFuture<List<AlignedImage>> manualElasticRegistration(@NotNull final AnalyzableImage targetImage,
