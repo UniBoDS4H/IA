c0ce16c14e51c0c6d4b6c9d938f205b2b1437fc6
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-10 09:36:39 +0200

    refactor: change function signature

--- src/main/java/com/ds4h/model/deformation/elastic/ElasticRegistration.java
+++ src/main/java/com/ds4h/model/deformation/elastic/ElasticRegistration.java
@@ -4,16 +4,17 @@ import com.ds4h.model.image.AnalyzableImage;
 import com.ds4h.model.image.alignedImage.AlignedImage;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 public interface ElasticRegistration {
     /**
      * Perform an elastic registration using the two input images.
-     * @param movingImage moving image that will be subject to the registration.
+     * @param movingImages moving images that will be subject to the registration.
      * @param targetImage target image, this will be fixed.
      * @return A future containing the transformed image.
      */
     @NotNull
-    CompletableFuture<AlignedImage> transform(@NotNull AnalyzableImage movingImage, @NotNull AnalyzableImage targetImage);
+    CompletableFuture<List<AnalyzableImage>> transformImages(@NotNull final AnalyzableImage targetImage, @NotNull final List<AnalyzableImage> movingImages);
 }
