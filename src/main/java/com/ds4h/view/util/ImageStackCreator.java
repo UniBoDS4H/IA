3508d6009b4a1533cb4ba273c97386c50f464ef1
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-10 10:09:16 +0200

    feat: init image stack creator

---
+++ src/main/java/com/ds4h/view/util/ImageStackCreator.java
@@ -0,0 +1,17 @@
+package com.ds4h.view.util;
+
+import com.ds4h.model.image.AnalyzableImage;
+import com.ds4h.model.image.alignedImage.AlignedImage;
+import ij.ImagePlus;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ImageStackCreator {
+    private ImageStackCreator() {}
+
+    @NotNull
+    public static ImagePlus createImageStack(@NotNull final List<AlignedImage> alignedImageList) {
+        return null;
+    }
+}
