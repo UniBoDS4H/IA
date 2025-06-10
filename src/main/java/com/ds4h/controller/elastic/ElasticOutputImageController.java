16c07d649629e46d296025502997a82aec66cf21
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-10 11:05:08 +0200

    feat: create ElasticOutputImage controller

---
+++ src/main/java/com/ds4h/controller/elastic/ElasticOutputImageController.java
@@ -0,0 +1,48 @@
+package com.ds4h.controller.elastic;
+
+import com.drew.lang.annotations.NotNull;
+import com.ds4h.controller.alignmentController.AlignmentControllerInterface;
+import com.ds4h.model.image.alignedImage.AlignedImage;
+import com.ds4h.view.util.ImageStackCreator;
+import ij.ImagePlus;
+
+import java.util.List;
+
+public class ElasticOutputImageController implements AlignmentControllerInterface {
+    private final List<AlignedImage> alignedImageList;
+
+    public ElasticOutputImageController(@NotNull final List<AlignedImage> alignedImageList) {
+        this.alignedImageList = alignedImageList;
+    }
+
+    @Override
+    public List<AlignedImage> getAlignedImages() {
+        return this.alignedImageList;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return false;
+    }
+
+    @Override
+    public String name() {
+        return "ELASTIC";
+    }
+
+    @Override
+    public ImagePlus getAlignedImagesAsStack() {
+        return ImageStackCreator.createImageStack(alignedImageList);
+    }
+
+    @Override
+    public int getStatus() {
+        return 0;
+    }
+
+    @Override
+    public void releaseImages() {
+        this.alignedImageList.clear();
+        System.gc();
+    }
+}
