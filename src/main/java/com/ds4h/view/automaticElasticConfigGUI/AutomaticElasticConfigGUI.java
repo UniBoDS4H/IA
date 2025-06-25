185e201c6fd34591ad9f55a39323adf585ca38b1
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-25 14:47:10 +0200

    feat: init combo for algorithms and init controller

--- src/main/java/com/ds4h/view/automaticElasticConfigGUI/AutomaticElasticConfigGUI.java
+++ src/main/java/com/ds4h/view/automaticElasticConfigGUI/AutomaticElasticConfigGUI.java
@@ -1,10 +1,23 @@
 package com.ds4h.view.automaticElasticConfigGUI;
 
+import com.drew.lang.annotations.NotNull;
+import com.ds4h.controller.elastic.ElasticController;
+import com.ds4h.model.alignment.automatic.pointDetector.Detectors;
+import com.ds4h.model.deformation.ElasticAlgorithms;
+import com.ds4h.view.automaticAlignmentConfigGUI.AutomaticAlignmentConfigGUI;
 import com.ds4h.view.standardGUI.StandardGUI;
 
 import javax.swing.*;
 
 public class AutomaticElasticConfigGUI extends JFrame implements StandardGUI {
+    private final JComboBox<ElasticAlgorithms> algorithms;
+    private final ElasticController controller;
+
+    public AutomaticElasticConfigGUI(@NotNull final ElasticController controller) {
+        this.controller = controller;
+        this.algorithms = new JComboBox<>(this.controller.getAlgorithms());
+    }
+
     @Override
     public void showDialog() {
 
