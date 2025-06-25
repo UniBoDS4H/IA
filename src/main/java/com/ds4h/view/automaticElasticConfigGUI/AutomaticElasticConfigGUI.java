9eb91c529ef8ce797ad424077175ef54c243bfd2
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-25 14:38:50 +0200

    feat: extends JFrame and StandardGui

--- src/main/java/com/ds4h/view/automaticElasticConfigGUI/AutomaticElasticConfigGUI.java
+++ src/main/java/com/ds4h/view/automaticElasticConfigGUI/AutomaticElasticConfigGUI.java
@@ -1,4 +1,22 @@
 package com.ds4h.view.automaticElasticConfigGUI;
 
-public class AutomaticElasticConfigGUI {
+import com.ds4h.view.standardGUI.StandardGUI;
+
+import javax.swing.*;
+
+public class AutomaticElasticConfigGUI extends JFrame implements StandardGUI {
+    @Override
+    public void showDialog() {
+
+    }
+
+    @Override
+    public void addListeners() {
+
+    }
+
+    @Override
+    public void addComponents() {
+
+    }
 }
