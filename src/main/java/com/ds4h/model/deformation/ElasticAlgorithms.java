ba556f4d8353c20324c55f55228eb0df2407ea83
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-26 14:46:07 +0200

    feat: add elastic registration algorithm inside the constructor

--- src/main/java/com/ds4h/model/deformation/ElasticAlgorithms.java
+++ src/main/java/com/ds4h/model/deformation/ElasticAlgorithms.java
@@ -1,17 +1,26 @@
 package com.ds4h.model.deformation;
 
 import com.drew.lang.annotations.NotNull;
+import com.ds4h.model.deformation.elastic.ElasticRegistration;
+import com.ds4h.model.deformation.elastic.ElasticRegistrationImpl;
 
 public enum ElasticAlgorithms {
-    BUNWARPJ("BunwarpJ"),
-    BIGWARP("BigWarp");
+    BUNWARPJ("BunwarpJ", new BunwarpjDeformation()),
+    BIGWARP("BigWarp", new ElasticRegistrationImpl());
     private final String name;
-    ElasticAlgorithms(@NotNull final String name) {
+    private final ElasticRegistration algorithm;
+    ElasticAlgorithms(@NotNull final String name, @NotNull final ElasticRegistration registration) {
         this.name = name;
+        this.algorithm = registration;
     }
 
     @NotNull
     public String getName() {
         return this.name;
+    }
+
+    @NotNull
+    public ElasticRegistration getAlgorithm() {
+        return this.algorithm;
     }
 }
