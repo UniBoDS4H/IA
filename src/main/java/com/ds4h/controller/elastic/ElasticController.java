147f3ad9f95b8192724e66df67794b64bb0484b0
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-26 14:46:20 +0200

    feat: get and use correct algorithm by selecting the one from the input

--- src/main/java/com/ds4h/controller/elastic/ElasticController.java
+++ src/main/java/com/ds4h/controller/elastic/ElasticController.java
@@ -9,16 +9,13 @@ import com.ds4h.model.deformation.elastic.ElasticRegistrationImpl;
 import com.ds4h.model.image.alignedImage.AlignedImage;
 import com.ds4h.model.pointManager.ImageManager;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ElasticController {
     private ElasticAlgorithms currentAlgorithm;
-    private final ElasticRegistration elasticRegistration;
     public ElasticController() {
-        this.elasticRegistration = new ElasticRegistrationImpl();
         this.currentAlgorithm = ElasticAlgorithms.BIGWARP;
     }
 
@@ -31,13 +28,14 @@ public class ElasticController {
     @NotNull
     public CompletableFuture<List<AlignedImage>> automaticElasticRegistration(@NotNull final ImageManager imageManager,
                                                                               @NotNull final Detectors detector) {
+        final ElasticRegistration registration = this.currentAlgorithm.getAlgorithm();
         final List<AlignedImage> outputImages = new LinkedList<>();
         final PointDetector pointDetector = detector.pointDetector();
         return CompletableFuture.supplyAsync(() -> {
             imageManager.getTargetImage().ifPresent(targetImage -> {
                 imageManager.getImagesToAlign().forEach(image -> {
                     pointDetector.detectPoint(targetImage, image);
-                    final AlignedImage transformedImage = this.elasticRegistration.transformImage(image, targetImage);
+                    final AlignedImage transformedImage = registration.transformImage(image, targetImage);
                     outputImages.add(transformedImage);
                     targetImage.clearPoints();
                     image.clearPoints();
@@ -56,10 +54,11 @@ public class ElasticController {
     @NotNull
     public CompletableFuture<List<AlignedImage>> manualElasticRegistration(@NotNull final ImageManager imageManager) {
         final List<AlignedImage> outputImages = new LinkedList<>();
+        final ElasticRegistration registration = this.currentAlgorithm.getAlgorithm();
         return CompletableFuture.supplyAsync(() -> {
             imageManager.getTargetImage().ifPresent(targetImage -> {
                 imageManager.getImagesToAlign().forEach(image -> {
-                    final AlignedImage alignedImage = this.elasticRegistration.transformImage(image, targetImage);
+                    final AlignedImage alignedImage = registration.transformImage(image, targetImage);
                     outputImages.add(alignedImage);
                 });
                 outputImages.add(new AlignedImage(targetImage.getImagePlus().getProcessor(), targetImage.getName()));
@@ -72,7 +71,7 @@ public class ElasticController {
         return ElasticAlgorithms.values();
     }
 
-    public void setAlgorithms(@NotNull ElasticAlgorithms algorithm) {
+    public void setAlgorithm(@NotNull ElasticAlgorithms algorithm) {
         this.currentAlgorithm = algorithm;
     }
 }
