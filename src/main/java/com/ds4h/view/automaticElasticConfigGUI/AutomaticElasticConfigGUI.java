58bd259b082454480f8ea6fabc6f8935f34ccc4f
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-26 14:25:42 +0200

    feat: update Dimension, use constant label

--- src/main/java/com/ds4h/view/automaticElasticConfigGUI/AutomaticElasticConfigGUI.java
+++ src/main/java/com/ds4h/view/automaticElasticConfigGUI/AutomaticElasticConfigGUI.java
@@ -4,6 +4,7 @@ import com.drew.lang.annotations.NotNull;
 import com.ds4h.controller.elastic.ElasticController;
 import com.ds4h.model.deformation.ElasticAlgorithms;
 import com.ds4h.view.standardGUI.StandardGUI;
+import com.ds4h.view.util.Constants;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,7 +17,7 @@ public class AutomaticElasticConfigGUI extends JFrame implements StandardGUI {
         this.setTitle("Automatic Elastic Config");
         this.controller = controller;
         this.algorithms = new JComboBox<>(this.controller.getAlgorithms());
-        this.setSize(new Dimension(250, 80));
+        this.setSize(new Dimension(300, 80));
         this.getContentPane().setLayout(new GridBagLayout());
         this.addComponents();
         this.addListeners();
@@ -40,14 +41,14 @@ public class AutomaticElasticConfigGUI extends JFrame implements StandardGUI {
         final GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5);
 
-        final JLabel algLbl = new JLabel("Elastic Algorithm: ");
-        gbc.gridx = 0;
+        final JLabel algLbl = new JLabel(Constants.ELASTIC_ALGORITHM_LABEL);
+        gbc.gridx = 1;
         gbc.gridy = 0;
         gbc.anchor = GridBagConstraints.LINE_START;
         this.add(algLbl, gbc);
 
-        gbc.gridx = 1;
-        gbc.gridwidth = 2;
+        gbc.gridx = 2;
+        gbc.gridwidth = 1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.weightx = 1.0;
         this.add(this.algorithms, gbc);
