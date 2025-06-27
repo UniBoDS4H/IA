890d729de1c8d0792540f63717a0de9533e9f6b1
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-27 14:12:35 +0200

    feat: set selected detector inside constructor

--- src/main/java/com/ds4h/view/outputGUI/AlignmentOutputGUI.java
+++ src/main/java/com/ds4h/view/outputGUI/AlignmentOutputGUI.java
@@ -1,5 +1,6 @@
 package com.ds4h.view.outputGUI;
 
+import com.drew.lang.annotations.NotNull;
 import com.ds4h.controller.elastic.ElasticOutputImageController;
 import com.ds4h.controller.imageController.ImageController;
 import com.ds4h.controller.pointController.ImageManagerController;
@@ -41,11 +42,14 @@ public class AlignmentOutputGUI extends StackWindow {
     private final MainMenuGUI mainGUI;
     private final boolean isOrderAscending;
     private final boolean isTargetImageForeground;
+    private final Detectors selectedDetector;
 
     public AlignmentOutputGUI(final ImageController controller, final BunwarpjGUI settingsBunwarpj,
                               final ImageManagerController imageManagerController, final MainMenuGUI mainMenuGUI,
-                              final boolean isAlignmentOrderAscending, final boolean isTargetImageForeground) {
+                              final boolean isAlignmentOrderAscending, final boolean isTargetImageForeground,
+                              @NotNull Detectors selectedDetector) {
         super(image = controller.getAlignedImagesAsStack(), new StandardCanvas(image));
+        this.selectedDetector = selectedDetector;
         this.canvas = (StandardCanvas)this.getCanvas();
         this.removeAll();
         this.controller = controller;
@@ -152,7 +156,7 @@ public class AlignmentOutputGUI extends StackWindow {
         });
 
         this.elasticItem.addActionListener(event -> {
-            this.controller.elastic(imageManagerController.getImageManager(), Detectors.SIFT)
+            this.controller.elastic(imageManagerController.getImageManager(), this.selectedDetector)
                     .whenComplete((images, ex) -> {
                         final ElasticOutputImageController elasticOutputImageController = new ElasticOutputImageController(images);
                         final ImageController imageController = new ImageController(elasticOutputImageController, this.controller.getElasticController());
@@ -161,7 +165,8 @@ public class AlignmentOutputGUI extends StackWindow {
                                 this.imageManagerController,
                                 this.mainGUI,
                                 this.isOrderAscending,
-                                this.isTargetImageForeground);
+                                this.isTargetImageForeground,
+                                this.selectedDetector);
                         this.dispose();
                     });
         });
