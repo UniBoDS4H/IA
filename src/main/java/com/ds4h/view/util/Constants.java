bb4f68eea67f1ca20bdefcfeb760b57d649c27c7
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-07-15 15:43:42 +0200

    feat: add label text

--- src/main/java/com/ds4h/view/util/Constants.java
+++ src/main/java/com/ds4h/view/util/Constants.java
@@ -2,5 +2,6 @@ package com.ds4h.view.util;
 
 public class Constants {
     public static String ELASTIC_ALGORITHM_LABEL = "Automatic Elastic Algorithm: ";
+    public static String ELASTIC_SLIDER_LABEL = "Nr of landmarks (BigWarp): ";
     public static String TOOLTIP_EMPTY_CONTAINER = "<html> You have to load at least 2 images</html>";
 }
