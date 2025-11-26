429055ccf6356595f52cb2df0e82e1766ab3c6b9
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-11-26 09:57:03 +0100

    feat: annotate with NotNull, remove redundant if

--- src/main/java/com/ds4h/model/alignment/AlignmentUtil.java
+++ src/main/java/com/ds4h/model/alignment/AlignmentUtil.java
@@ -1,5 +1,6 @@
 package com.ds4h.model.alignment;
 
+import com.drew.lang.annotations.NotNull;
 import com.ds4h.model.alignment.alignmentAlgorithm.*;
 
 import java.util.Objects;
@@ -19,17 +20,14 @@ public class AlignmentUtil {
      * @param alg the selected algorithm.
      * @return the algorithm that will be used for the alignment.
      */
-    public static AlignmentAlgorithmEnum getEnumFromAlgorithm(final AlignmentAlgorithm alg){
-        if(Objects.nonNull(alg)) {
-            if (alg instanceof TranslationalAlignment) {
-                return AlignmentAlgorithmEnum.TRANSLATIONAL;
-            } else if (alg instanceof ProjectiveAlignment) {
-                return AlignmentAlgorithmEnum.PROJECTIVE;
-            } else if (alg instanceof AffineAlignment) {
-                return AlignmentAlgorithmEnum.AFFINE;
-            }
-            throw new NullPointerException("Algorithm not present");
+    @NotNull
+    public static AlignmentAlgorithmEnum getEnumFromAlgorithm(@NotNull final AlignmentAlgorithm alg){
+        if (alg instanceof TranslationalAlignment) {
+            return AlignmentAlgorithmEnum.TRANSLATIONAL;
+        } else if (alg instanceof ProjectiveAlignment) {
+            return AlignmentAlgorithmEnum.PROJECTIVE;
+        } else if (alg instanceof AffineAlignment) {
+            return AlignmentAlgorithmEnum.AFFINE;
         }
-        throw new NullPointerException("Algorithm is NULL");
     }
 }
