e6271647ae0218e2c969d17c30b13a4e278a2a02
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-04 19:53:31 +0100

    feat: create new factory method for creating image point from file path

--- src/main/java/com/ds4h/model/image/imagePoints/ImagePointsFactory.java
+++ src/main/java/com/ds4h/model/image/imagePoints/ImagePointsFactory.java
@@ -1,4 +1,12 @@
 package com.ds4h.model.image.imagePoints;
 
+import com.drew.lang.annotations.NotNull;
+
 public class ImagePointsFactory {
+
+    @NotNull
+    public static ImagePoints createFromFilePath(@NotNull final String filePath) {
+        return new ImagePoints(filePath);
+    }
+
 }
