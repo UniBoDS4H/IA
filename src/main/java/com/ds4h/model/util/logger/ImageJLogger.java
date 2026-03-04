7a420189d56b4d67cd3ef66cb3e1fb02d02144f1
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-04 19:42:37 +0100

    feat: add implementation for all logs methods

--- src/main/java/com/ds4h/model/util/logger/ImageJLogger.java
+++ src/main/java/com/ds4h/model/util/logger/ImageJLogger.java
@@ -1,18 +1,39 @@
 package com.ds4h.model.util.logger;
 
+import com.drew.lang.annotations.NotNull;
+import ij.IJ;
+
 public class ImageJLogger implements Logger {
+    @NotNull private final static String ERROR = "ERROR", WARNING = "WARNING", INFO = "INFO";
+    @NotNull private final String myClassName;
+
+    public ImageJLogger(@NotNull final String className) {
+        myClassName = className;
+    }
+
     @Override
     public void log(String message) {
-
+        if (Boolean.getBoolean(System.getProperty(LoggerFactory.DEBUG_PROPERTY))) {
+            IJ.log(formatMessage(INFO, message));
+        }
     }
 
     @Override
     public void logError(String message) {
-
+        if (Boolean.getBoolean(System.getProperty(LoggerFactory.DEBUG_PROPERTY))) {
+            IJ.showMessage(formatMessage(ERROR, message));
+        }
     }
 
     @Override
     public void logWarning(String message) {
+        if (Boolean.getBoolean(System.getProperty(LoggerFactory.DEBUG_PROPERTY))) {
+            IJ.showMessage(formatMessage(WARNING, message));
+        }
+    }
 
+    @NotNull
+    private String formatMessage(@NotNull final String logType, @NotNull final String message) {
+        return String.format("[ %s ] %s: current message: %s", myClassName, logType, message);
     }
 }
