b03e6ce01d26525d329df57bc01a16d9f02264a3
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-05 11:34:06 +0100

    refactor: use internal logger, enhance method quality

--- src/main/java/com/ds4h/model/util/MemoryController.java
+++ src/main/java/com/ds4h/model/util/MemoryController.java
@@ -1,6 +1,9 @@
 package com.ds4h.model.util;
 
+import com.drew.lang.annotations.NotNull;
 import com.ds4h.model.image.imagePoints.ImagePoints;
+import com.ds4h.model.util.logger.Logger;
+import com.ds4h.model.util.logger.LoggerFactory;
 import com.sun.management.OperatingSystemMXBean;
 import ij.IJ;
 
@@ -11,7 +14,7 @@ import java.util.List;
  * This class is used in order to check the memory when the images are loaded inside the Plugin.
  */
 public class MemoryController {
-
+    private final static Logger myLogger = LoggerFactory.getImageJLogger(MemoryController.class);
     private final static double MEMORY_PERCENTAGE_LIMIT = 30.0;
 
     private MemoryController(){
@@ -23,14 +26,13 @@ public class MemoryController {
      * @param imagePoints the images loaded from the Plugin.
      * @throws OutOfMemoryError if the 30% of the memory is used by the images.
      */
-    public static void controlMemory(final List<ImagePoints> imagePoints) throws OutOfMemoryError{
-        long memorySize = 0;
+    public static void controlMemory(@NotNull final List<ImagePoints> imagePoints) throws OutOfMemoryError{
         final long totalMemory = (IJ.maxMemory()/(1024*1024));
         final long currentMemory = (IJ.currentMemory() / (1024 * 1024));
         final double usedMemory = ((double) currentMemory / totalMemory) * 100;
-        IJ.log("[MEMORY SIZE IMAGE] TotalSize: " + currentMemory + " MB");
-        IJ.log("[MEMORY SIZE IMAGE] Total Memory: " + totalMemory + " MB");
-        IJ.log("[MEMORY SIZE IMAGE] Percentage: " + usedMemory + " %");
+        myLogger.log("Total Size: " + currentMemory + " MB");
+        myLogger.log("Total Memory: " + totalMemory+ " MB");
+        myLogger.log("Percentage: " + usedMemory+ " %");
         if(usedMemory >= MEMORY_PERCENTAGE_LIMIT){
             throw new OutOfMemoryError(MemoryController.getError());
         }
@@ -41,10 +43,10 @@ public class MemoryController {
      * @return the error message.
      */
     private static String getError(){
-        OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
-        long physicalMemorySize = osBean.getTotalPhysicalMemorySize() -
+        final OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+        final long physicalMemorySize = osBean.getTotalPhysicalMemorySize() -
                 (osBean.getFreePhysicalMemorySize() + osBean.getFreeSwapSpaceSize()) ;
-        long ramSizeGB = physicalMemorySize / (1024 * 1024);
+        final long ramSizeGB = physicalMemorySize / (1024 * 1024);
         return "The memory is not enough.\n"+
                 "Please, expand your memory to use this Plugin.\n " +
                 "You can expand the memory from the Fiji/ImageJ menu:\n" +
