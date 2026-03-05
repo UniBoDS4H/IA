ea6e5cab1d630960ffe827e652abff2aacb55be8
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-05 11:32:11 +0100

    feat: use internal logger

--- src/main/java/com/ds4h/model/util/ImageStackCreator.java
+++ src/main/java/com/ds4h/model/util/ImageStackCreator.java
@@ -1,6 +1,8 @@
 package com.ds4h.model.util;
 
 import com.ds4h.model.image.alignedImage.AlignedImage;
+import com.ds4h.model.util.logger.Logger;
+import com.ds4h.model.util.logger.LoggerFactory;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
@@ -10,6 +12,7 @@ import java.awt.image.ColorModel;
 import java.util.List;
 
 public class ImageStackCreator {
+    private final static Logger myLogger = LoggerFactory.getImageJLogger(ImageStackCreator.class);
     private ImageStackCreator() {}
 
     @NotNull
@@ -17,12 +20,13 @@ public class ImageStackCreator {
         final ImagePlus alignedImage = alignedImageList.get(0).getAlignedImage();
         final ImageStack stack = new ImageStack(alignedImage.getWidth(), alignedImage.getHeight(), ColorModel.getRGBdefault());
         for (final AlignedImage image : alignedImageList) {
-            IJ.log("[STACK CREATION] Current image: " + image.getName());
+            myLogger.log("Current image stack: " + image.getName());
             stack.addSlice(image.getName(), image.getProcessor());
         }
         try {
             return new ImagePlus("AlignedStack", stack);
         }catch (final Exception e){
+            myLogger.logError("Something went wrong with the creation of the stack: " + e.getMessage());
             throw new RuntimeException("Something went wrong with the creation of the stack.\n" +
                     "Error: " + e.getMessage());
         }
