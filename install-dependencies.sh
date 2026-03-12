60100324dc681a5462a67bc069203da7bdaf71e5
matteo.iorio2 <matteo.iorio01@gmail.com>
2026-03-12 10:28:45 +0100

    chore: remove install depdencies script

--- install-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DFILE="$(pwd)/src/main/resources/opencv/library/opencv-455.jar"
-
-mvn install:install-file \
-  -Dfile="$DFILE" \
-  -DgroupId=ds4h.org.opencv \
-  -DartifactId=opencv \
-  -Dversion=4.5.5 \
-  -Dpackaging=jar
